80e1db42c478c8064ecb50d161493611767d8322
Luiz Guilherme Paro <lg_paro@hotmail.com>
2019-02-03 16:11:01 -0600

    refactoring jokes app to allow multiple joke servers

---
+++ 03-refactored-jokes-app-to-allow-multiple-jokeservers/academy.learnprogramming.jokeserver.programmer/src/module-info.java
@@ -0,0 +1,6 @@
+module academy.learnprogramming.jokeserver.programmer {
+    requires academy.learnprogramming.jokeserver;
+    requires java.sql;
+
+    exports academy.learnprogramming.jokeserver.programmer;
+}
